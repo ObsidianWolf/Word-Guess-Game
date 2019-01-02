fd6b6884363c1485e1fcf3686443069af2e4d438
Your Name <you@example.com>
2019-01-01 20:53:25 -0500

    completed some basic html and css styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+console.log("sanity")
+
+//take user click of start
+
+//start the game
+
+//T0
+//we need a random word
+//display underscores for each letter
+//display wins and losses
+//display how many guesses are left
+//create a listener for user inputing letters
+//if the letter is in the word 
+    //if true 
+        //replace the correct underscore with the letter
+        //hint*
+            //you may have to change multiple unscores with the first guess
+        //did you win 
+        //if user wins reset game and incrimit score by 1..
+    //if false
+        //subtract from guesses remaining by 1..
+        //did you lose
+        //if user loses game over, reset game increase losses.
+        //keep track of the letters guessed.
